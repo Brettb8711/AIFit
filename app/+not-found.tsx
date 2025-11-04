cc09d3b678f22cb4415dc5df84da155a9cbcb12b
Brett Burton <brett10010@gmail.com>
2025-11-03 19:38:24 -0700

    Added Bottom Navigation

---
+++ app/+not-found.tsx
@@ -0,0 +1,30 @@
+import { Link } from 'expo-router';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function NotFound() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>404 - Not Found</Text>
+      <Link href="../" style={styles.button}>
+        Go back to Home
+      </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: '#fff',
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: '#fff',
+  },
+});
