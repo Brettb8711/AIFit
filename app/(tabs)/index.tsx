f7dc2aed2060b45903341de642f4507ff97d86ff
Brett Burton <brett10010@gmail.com>
2026-03-03 21:36:17 -0700

    added navigation

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,32 @@
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Home Screen</Text>
+      <Link href="/about" style={styles.link}>
+        Go to About
+      </Link>
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 18,
+    color: "#fff",
+  },
+  link: {
+    color: "#fff",
+    fontSize: 20,
+    textDecorationLine: "underline"
+  }
+});
